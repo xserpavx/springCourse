24018555ed0d600aa70f640febe3d3673099e3db
roland <serpav@servista.ru>
2021-08-13 10:16:59 +0800

    Написал методы добавления, удаления объектов в cookie и добавления cookie в response

--- src/main/java/com/example/demo/controllers/PostponedController.java
+++ src/main/java/com/example/demo/controllers/PostponedController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.util.StringJoiner;
+import java.util.regex.Pattern;
 
 /**
  * Created on 21.07.2021
@@ -35,8 +36,49 @@ public class PostponedController {
         if (postponedBooks != null && !postponedBooks.isEmpty()) {
             String[] pb = postponedBooks.replaceAll("^/", "").replaceAll("/$", "").split("/");
             model.addAttribute("postponed", bookService.getBooksBySlug(pb));
+            model.addAttribute("slugs4BuyAll", postponedBooks);
         }
         return "postponed";
+    }
+
+    /** Удаляет из куки список передаваемых значений
+     * @param cookieValue текущее значение куки
+     * @param removeCookieValues список удаляемых значений
+     * @return новое значение куки
+     */
+    private String removeCookieValue(String cookieValue, String removeCookieValues) {
+        String result = cookieValue != null ? cookieValue : "";
+        String[] removeValues = removeCookieValues.split("/");
+        for (String remove : removeValues) {
+            // Удаляем значение из начала строки
+            result = result.replaceFirst("^"+remove+"/?(.*)?", "$1");
+            // Удаляем значение из конца строки
+            result = result.replaceFirst("^(.*)/e2$", "$1");
+            // Удаляем значение из середины
+            result = result.replaceFirst("^(.*)/e2/?(.*)?", "$1$2");
+        }
+        return result;
+    }
+
+    private String addCookieValue(String cookieValue, String addCookieValues) {
+        var stringJoiner = new StringJoiner("/");
+        stringJoiner.add(cookieValue != null ? cookieValue : "");
+        String[] addValues = addCookieValues.split("/");
+        for (String add : addValues) {
+            var matcher = Pattern.compile("(.*?/"+add+"$)|(^"+add+"/.*?)|(.*?/"+add+"/.*?)").matcher(stringJoiner.toString());
+            if (!matcher.matches()) {
+                stringJoiner.add(add);
+            }
+        }
+        return stringJoiner.toString().replaceAll("^/", "");
+    }
+
+    private void addCookie(String cookieName, String cookieValue, String cookiePath, HttpServletResponse response) {
+        if (!cookieValue.isEmpty()) {
+            Cookie cookie = new Cookie(cookieName, cookieValue);
+            cookie.setPath(cookiePath);
+            response.addCookie(cookie);
+        }
     }
 
     @PostMapping(value="/books/changeBookStatus")
@@ -45,29 +87,20 @@ public class PostponedController {
                                    @CookieValue(name="cartBooks", required = false) String cartBooks,
                                    HttpServletResponse response, Model model
     ) {
-        StringJoiner stringJoiner = new StringJoiner("/");
         BookRepository.BookUserTypes switchBy = BookRepository.BookUserTypes.valueOf(status);
         String cookieName = "";
+        String result = "";
         switch (switchBy) {
             case KEPT:
-                if (postponedBooks == null || postponedBooks.length() == 0) {
-                    stringJoiner.add(slug);
-                } else if (!postponedBooks.contains(slug)) {
-                    stringJoiner.add(postponedBooks).add(slug);
-                }
-                model.addAttribute("isPostponedEmpty", false);
-                cookieName = "postponedBooks";
-                break;
+                addCookie("postponedBooks",  addCookieValue(postponedBooks, slug), "/books", response);
+                addCookie("cartBooks",  removeCookieValue(cartBooks, slug), "/books", response);
+                return "redirect:/books/"+slug;
             case CART:
+                // При добавлении книг в cookie корзины - необходимо убрать их из cookie отложено
                 cookieName = "cartBooks";
                 break;
         }
 
-        if (!stringJoiner.toString().isEmpty()) {
-            Cookie cookie = new Cookie(cookieName, stringJoiner.toString());
-            cookie.setPath("/books");
-            response.addCookie(cookie);
-        }
-        return "redirect:/books/"+slug;
+        return result;
     }
 }
